9c8f032a16fcd5390f2603572042522d7bbacbe1
amitwadhwa83 <38137402+amitwadhwa83@users.noreply.github.com>
2018-04-06 21:54:44 +0530

    Initail submit
    Initail submit

---
+++ players/src/main/java/com/sample/players/LaunchPlayer.java
@@ -0,0 +1,24 @@
+package com.sample.players;
+
+/**
+ * This class is entry point for launching/starting the process of message
+ * communication between Player(s)
+ *
+ */
+public class LaunchPlayer {
+    public static void main(String args[]) {
+
+	// Starting initiator in separate thread to start communication between players
+	// in the same java process
+	new Thread() {
+	    public void run() {
+		Player initiator = new Player();
+		initiator.startInitiator();
+	    }
+	}.start();
+
+	// Starting receiver
+	Player receiver = new Player();
+	receiver.startReceiver();
+    }
+}
